be122255037eeb2af9e02c3d90cb362ded8207d7
Pembo97 <30778935+Pembo97@users.noreply.github.com>
2019-03-03 22:25:03 +0000

    Update project_euler_3.cpp

--- project_euler_3.h
+++ project_euler_3.h
@@ -2,9 +2,21 @@
 
 bool is_prime(long long int n)
 {
-
 	for (int i = 2; i <= sqrt(n); ++i)
-		if ((n % i) == 0) return false;
+	if ((n % i) == 0) return false;
 
-	return true;
+    return true;
+}
+
+int main()
+{
+	long long int num = 600851475143;
+	long long int high = 2;
+	for (long long int i = 2; i <= sqrt(num); ++i) {
+		if ((num % i) == 0)
+			if (is_prime(i))
+				high = i;
+	}
+
+	cout << high << endl;
 }
